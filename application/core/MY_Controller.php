8b581ce00f7bd312ab660bd7800583a580307073
abenson <aaron.benson@jwt.com>
2012-03-08 11:22:10 -0600

    Changed accessors for controller super class.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,30 +10,30 @@ class MY_Controller extends CI_Controller
     parent::__construct();
   }
   
-  public function get_user_data()
+  protected function get_user_data()
   {
     return $this->session->all_userdata();
   }
   
-  public function get_user_id()
+  protected function get_user_id()
   {
     $session_data = $this->session->all_userdata();
     return $session_data['user_id'];
   }
   
-  public function is_logged_in()
+  protected function is_logged_in()
   {
     $session_data = $this->session->all_userdata();
     return (isset($session_data['user_id']) && $session_data['user_id'] > 0 && isset($session_data['logged_in']) && $session_data['logged_in'] === TRUE);
   }
   
-  public function is_admin()
+  protected function is_admin()
   {
     $session_data = $this->session->all_userdata();
     return (isset($session_data['is_admin']) && $session_data['is_admin'] == 1);
   }
   
-  public function create_uuid()
+  protected function create_uuid()
   {
     $uuid_query = $this->db->query('SELECT UUID()');
     $uuid_rs = $uuid_query->result_array();
